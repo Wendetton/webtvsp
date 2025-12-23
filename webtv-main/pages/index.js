61ad5db4157479b09c56764b34b68b785384a6c3
Wendetton <feazegoncalves@gmail.com>
2025-12-23 11:40:51 -0300

    Delete webtv-main directory

--- webtv-main/pages/index.js
+++
@@ -1,8 +0,0 @@
-// Redireciona para painel administrativo por padrão
-import { useEffect } from "react";
-export default function Home() {
-  useEffect(() => {
-    window.location.href = "/admin";
-  }, []);
-  return <div>Redirecionando para painel administrativo...</div>;
-}
